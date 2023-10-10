a32654f24058072272ccc25b9db9036bfe7e6771
Devon <ds.pydev@gmail.com>
2023-10-10 11:54:04 +0200

    Add optional exit during loop

--- barcode.py
+++ barcode.py
@@ -2,14 +2,16 @@
 # x = "2000000025476"
 # x = "2000000025278"
 
-# CHECK IF BARCODE 12 OR 13 DIGIT // ERROR HANDELING // GIT UPLOAD
-
 a = 'yes'
 
 while a == 'y' or a == 'yes':
     while True:
         try:
+            print('=== To exit please type 0 (Zero) ===')
             x = int(input('Please enter 12 or 13 digit barcode in full: '))
+
+            if x == 0:
+                break
             
             x = str(x)
             
@@ -22,27 +24,30 @@ while a == 'y' or a == 'yes':
             print('=== Please enter a valid number ===')
             continue
 
-    # # Split barcode number in to list
-    # y = [int(i) for i in list(x)]
+    if x == 0:
+        break
 
-    # # Calculate sum of odd number placements
-    # barcode_odd = sum(y[1::2])*3
+    # Split barcode number in to list
+    y = [int(i) for i in list(x)]
 
-    # # Calculate sum of even number placements
-    # barcode_even = sum(y[:-1:2])
+    # Calculate sum of odd number placements
+    barcode_odd = sum(y[1::2])*3
 
-    # # Add sum of odd and even
-    # check_digit_sum = barcode_odd + barcode_even
+    # Calculate sum of even number placements
+    barcode_even = sum(y[:-1:2])
 
-    # # Round sum to nearest 10
-    # check_digit_round = rounded = round(check_digit_sum/10)*10
+    # Add sum of odd and even
+    check_digit_sum = barcode_odd + barcode_even
 
-    # # Caluation to get check digit
-    # if check_digit_sum > check_digit_round:
-    #     check_digit = (check_digit_round + 10) - check_digit_sum
-    # else:
-    #     check_digit = check_digit_round - check_digit_sum
+    # Round sum to nearest 10
+    check_digit_round = rounded = round(check_digit_sum/10)*10
+
+    # Caluation to get check digit
+    if check_digit_sum > check_digit_round:
+        check_digit = (check_digit_round + 10) - check_digit_sum
+    else:
+        check_digit = check_digit_round - check_digit_sum
         
-    # print(f"check digit = {check_digit}")
+    print(f"check digit = {check_digit}")
 
     a = input('Would you like to contuine or exit (y for "Yes" and n for "No"): ')
